6f0b5caf513d008b4a67a2f1c192101e008129d5
imuzairahmad <uzairahmad0110@gmail.com>
2026-03-13 05:29:22 +0500

    stupit commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1>WHATSAPP AFFILATION BOT. :(</h1>
+      <h1>WHATSAPP AFFILATION BOT. :(</h1>{" "}
+      <p>Welcome to the WhatsApp Affiliate Bot!</p>
     </div>
   );
 }
